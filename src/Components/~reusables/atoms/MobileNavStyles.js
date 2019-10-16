186b3a6b6fb1a7ed3ed1f211adad44ed7ef67b7e
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-16 13:01:30 +0100

    add social icons

--- src/Components/~reusables/atoms/MobileNavStyles.js
+++ src/Components/~reusables/atoms/MobileNavStyles.js
@@ -1,31 +1,66 @@
-import styled from 'styled-components';
-import { red, mobileMAxWidth, tabletMaxWidth, white, smallSpace, heading4 } from '../variables'
+import styled from "styled-components";
+import {
+  red,
+  mobileMAxWidth,
+  tabletMaxWidth,
+  deskTopMaxWidth,
+  white,
+  smallSpace,
+  heading4
+} from "../variables";
 
 export const Navbar = styled.nav`
-height: 50px;
-background: ${red};
-display: none;
-.fa-bars {
-        font-size: ${heading4};
-        position: absolute;
-        right: ${smallSpace};
-        top: ${smallSpace};
-        color: ${white};
-        display: block;
-        cursor: pointer;
-        z-index: 5;
+  height: 50px;
+  background: ${red};
+  display: none;
+  .fa-bars {
+    font-size: ${heading4};
+    position: absolute;
+    right: ${smallSpace};
+    top: ${smallSpace};
+    color: ${white};
+    display: block;
+    cursor: pointer;
+    z-index: 5;
 
-        @media (max-width: ${tabletMaxWidth}) {
-            display: flex;
-        }
+    @media (max-width: ${tabletMaxWidth}) {
+      display: flex;
     }
+  }
 
-@media (max-width: ${tabletMaxWidth}) {
-            display: flex;
-        }
+  @media (max-width: ${tabletMaxWidth}) {
+    display: flex;
+  }
 
-@media (max-width: ${mobileMAxWidth}) {
+  @media (max-width: ${mobileMAxWidth}) {
     display: flex;
     width: 100%;
-    }
+  }
+`;
+
+export const NavItems = styled.div`
+  font-size: 18px;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  border-width: 1px 0;
+  padding: 0 20px;
+  display: none;
+  a {
+    display: inline-block;
+  }
+  @media screen and (max-width: ${deskTopMaxWidth}) {
+    display: block;
+  }
+`;
+
+export const Social = styled.div`
+  float: left;
+  margin-top: 0.5rem;
+  a {
+    color: rgba(255, 255, 255, 0.6);
+    padding: 0;
+    margin-right: 0.5em;
+    font-size: ${heading4};
+    display: inline-block;
+    cursor: pointer;
+  }
 `;
--- src/Components/~reusables/components/MobileNav.jsx
+++ src/Components/~reusables/components/MobileNav.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar } from "../atoms/MobileNavStyles";
+import { Navbar, Social, NavItems } from "../atoms/MobileNavStyles";
 import { MyContext } from "../../../Provider";
 
 class MobileNav extends Component {
@@ -8,12 +8,24 @@ class MobileNav extends Component {
       <MyContext.Consumer>
         {context => (
           <Navbar>
-            <i
-              className="fas fa-bars"
-              role="presentation"
-              onClick={context.toggleSidebar}
-            />
-            Navigation
+            <NavItems>
+              <Social>
+                <a rel="me" href='https://www.linkedin.com/in/pascal-ulor/' title="linkedin">
+                <i className="fab fa-linkedin"/>
+                </a>
+                <a rel="me" href='https://github.com/PascalUlor' title="github">
+                <i className="fab fa-github-square"/>
+                </a>
+                <a rel="me" href='https://twitter.com/pc_codes' title="github">
+                <i className="fab fa-twitter-square"/>
+                </a>
+              </Social>
+              <i
+                className="fas fa-bars"
+                role="presentation"
+                onClick={context.toggleSidebar}
+              />
+            </NavItems>
           </Navbar>
         )}
       </MyContext.Consumer>
